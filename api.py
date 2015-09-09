818ca985f4ac9d783c371b5a8de3ab02217df264
Jason Hamilton-Smith <hs.jason@gmail.com>
2015-09-08 21:40:50 -0700

    Got some basic upload working
    This will need to be redone to be actualy secure,
    just stopgap for now.
    
    "stopgap" is a dangerous word

--- api.py
+++ api.py
@@ -1,10 +1,34 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from werkzeug import secure_filename
+import os
 from model import Package
 
+
 api = Blueprint('api', __name__)
+
+UPLOAD_FOLDER = '/home/jason/apm_uploads'
+
 
 @api.route('/list_packages')
 def list_packages():
     packs = Package.query.all()
     return jsonify(packs_available = packs)
 
+@api.route('/upload_package', methods = ['GET', 'POST'])
+def upload_package():
+    if request.method == 'POST':
+       file = request.files['file']
+       if file:
+           filename = secure_filename(file.filename)
+           file.save(os.path.join(UPLOAD_FOLDER, filename))
+           return 'OK'
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+ 
